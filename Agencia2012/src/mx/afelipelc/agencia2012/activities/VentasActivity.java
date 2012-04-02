d50d199e54cc9d4456cdeb03709601ecd03a779c
afelipelc <afelipelc@gmail.com>
2012-04-02 15:31:59 -0500

    import android.Log no agregado, corregido aqui.

--- Agencia2012/src/mx/afelipelc/agencia2012/activities/VentasActivity.java
+++ Agencia2012/src/mx/afelipelc/agencia2012/activities/VentasActivity.java
@@ -6,6 +6,7 @@ import mx.afelipelc.agencia2012.adaptadores.AdaptadorVenta;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.ContextMenu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
